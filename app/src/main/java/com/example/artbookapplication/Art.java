2eea2700ea5646b3928335a8966b916861beceb6
Bünyamin Türkkan <berol16122002@gmail.com>
2021-08-25 20:50:44 +0300

    Art Book

---
+++ app/src/main/java/com/example/artbookapplication/Art.java
@@ -0,0 +1,13 @@
+package com.example.artbookapplication;
+
+public class Art {
+
+    String name;
+    int id;
+
+    public Art(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+}
